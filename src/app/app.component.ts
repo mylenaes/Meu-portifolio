67b6f00969dee34b1f994775ceb6cdd2f1ee27f3
Mylena <mylenasilvan.es@gmail.com>
2025-04-29 12:19:06 -0300

    front ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { TecnologiasComponent } from './components/tecnologias/tecnologias.component';
+import { ContatoComponent } from './components/contato/contato.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, HomeComponent, SobreComponent, TecnologiasComponent, ContatoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
